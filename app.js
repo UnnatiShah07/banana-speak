59efc88121495ba722b70fb60ebebf028c96ec07
UnnatiShah07 <76685572+UnnatiShah07@users.noreply.github.com>
2022-09-18 22:51:07 +0530

    Complete project

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-var btnTranslate = document.querySelector("#btn-translate");
+var btnTranslate = document.querySelector("#translate-button");
+var txtInput = document.querySelector("#translate-input");
+var output = document.querySelector("#translate-output");
 
-btnTranslate.addEventListener("click",function clickEventHandler(){
-    console.log("click");
-})
+const serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function generateURL(text) {
+    return serverURL + "?text=" + text;
+}
+
+function errorHandler(error) {
+    console.log(error);
+    alert("Somthing wrong with server. Please try again after somtime.");
+}
+
+function clickEventHandler() {
+    var inputText = txtInput.value;
+
+    fetch(generateURL(inputText))
+        .then((response) => response.json())
+        .then((json) => {
+            var translatedText = json.contents.translated;
+            output.innerHTML = translatedText;
+        })
+        .catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
